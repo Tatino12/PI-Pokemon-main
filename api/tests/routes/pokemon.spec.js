4ecca2638e9b1a0939023c5bf9851968a9334ae1
Tatino12 <marianohuecke@hotmail.com>
2022-02-15 13:35:09 -0300

    finished

--- api/tests/routes/pokemon.spec.js
+++ api/tests/routes/pokemon.spec.js
@@ -4,6 +4,8 @@ const session = require('supertest-session');
 const app = require('../../src/app.js');
 const { Pokemon, conn } = require('../../src/db.js');
 
+
+
 const agent = session(app);
 const pokemon = {
   name: 'Pikachu',
@@ -16,9 +18,24 @@ describe('Pokemon routes', () => {
   }));
   beforeEach(() => Pokemon.sync({ force: true })
     .then(() => Pokemon.create(pokemon)));
+
   describe('GET /pokemons', () => {
-    it('should get 200', () =>
-      agent.get('/pokemons').expect(200)
-    );
+    it('should get 200', () =>{
+      agent.get('/pokemons').expect(200);
+      
+    });
   });
 });
+
+
+describe('Types routes', () => {
+  before(() => conn.authenticate()
+  .catch((err) => { 
+    console.error('Unable to connect to the database:', err);
+  })
+  );
+  describe('GET /types', () => {
+    it('should get 200', () => agent.get('/types').expect(200))
+  })
+
+})
